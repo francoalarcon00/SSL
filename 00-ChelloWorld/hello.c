fd70123d00b8122ed62872b813483274080557bf
Franco Alarcon <falarcon@frba.utn.edu.ar>
2025-04-21 12:21:49 -0300

    Update hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    
-    FILE *file = fopen("output.txt", "w");
-    
-    if (file == NULL) {
-        perror("Error al abrir el archivo");
-        return 1;
-    }
 
-    fprintf(file, "Hola Framco Alarcon!\n");
-
-    fclose(file);
+    printf("Hola Franco Alarcon!\n");
     
     return 0;
 }
